0686b0c0e50f5717358b580d6eaebd220049c8af
mucahit-sahin <mucahitsahin786@gmail.com>
2020-10-06 19:38:48 +0300

    sidebar görünümü  tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>twitter clone</h1>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption/SidebarOption";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MessageIcon from "@material-ui/icons/Message";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ListIcon from "@material-ui/icons/List";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <SidebarOption active text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Search" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsIcon} />
+      <SidebarOption text="Messages" Icon={MessageIcon} />
+      <SidebarOption text="Bookmarks" Icon={BookmarkIcon} />
+      <SidebarOption text="Lists" Icon={ListIcon} />
+      <SidebarOption text="Profile" Icon={AccountCircleIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
+      <Button variant="outlined" className="sidebar-tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
