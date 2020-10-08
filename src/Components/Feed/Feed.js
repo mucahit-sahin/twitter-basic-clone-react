f6aa046834d66951b8788699e1dcee4ab325539a
mucahit-sahin <mucahitsahin786@gmail.com>
2020-10-08 19:13:56 +0300

    post componenti oluşturuldu

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Feed.css";
 import TweetBox from "./TweetBox/TweetBox";
-
+import Post from "./Post/Post";
 function Feed() {
   return (
     <div className="feed">
@@ -10,6 +10,29 @@ function Feed() {
       </div>
 
       <TweetBox />
+
+      <Post
+        userimage="https://avatars2.githubusercontent.com/u/38807255?s=460&u=deb087d587be7f6a4000e4e710ec4d1daa6fde84&v=4"
+        username="mucahitsah"
+        displayName="Mücahit Şahin"
+        text="Yeni Apple Watch Series 6 ve Apple Watch SE Türkiye'ye ön siparişe açıldı!
+
+        Apple Watch Series 6: 3.799 TL
+        Apple Watch Series 6 44 mm: 4.049 TL
+        Apple Watch SE 40 mm: 2.699 TL
+        Apple Watch SE 44 mm: 2.949 TL
+        "
+      />
+      <Post
+        userimage="https://avatars2.githubusercontent.com/u/38807255?s=460&u=deb087d587be7f6a4000e4e710ec4d1daa6fde84&v=4"
+        username="mucahitsah"
+        displayName="Mücahit Şahin"
+        text="Diego Perotti: “Fenerbahçe gibi önemli bir ekibe katılmak gerçekten
+        benim için de mutluluk verici. Kendi tecrübemle takıma katkıda
+        bulunacağıma inanıyorum. Takımın da kapasitesi oldukça geniş ve yüksek.
+        Bu kapasiteyle hep beraber başarılara ulaşacağımıza inanıyorum.”
+        "
+      />
     </div>
   );
 }
---
+++ src/Components/Feed/Post/Post.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./Post.css";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import CommentIcon from "@material-ui/icons/Comment";
+import RepeatIcon from "@material-ui/icons/Repeat";
+
+function Post({ userimage, username, displayName, text }) {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <Avatar src={userimage} />
+        <h3>{displayName}</h3>
+        <h4>{"@" + username}</h4>
+      </div>
+      <div className="post-content">{text}</div>
+      <div className="post-event">
+        <div>
+          <FavoriteBorderIcon />
+        </div>
+        <div>
+          <CommentIcon />
+        </div>
+        <div>
+          <RepeatIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
